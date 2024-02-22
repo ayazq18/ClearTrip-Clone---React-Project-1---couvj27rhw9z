a537697a428482681184ab1e5ec6e4a0a9ac9b1a
ayazq18@gmail.com <131868270+ayazq18@users.noreply.github.com>
2024-02-22 20:03:44 +0530

    updated

--- src/components/Maintenance/Maintenance.js
+++ src/components/Maintenance/Maintenance.js
@@ -1,8 +1,9 @@
 import React from 'react'
 export default function Maintenance() {
   return (
-    <div className='flexXY' style={{width:'100vw', height:'100vh', backgroundColor:'rgba(242, 246, 250, 0.753)'}}>
-    <div className='maintenance' style={{backgroundImage: 'url("https://cdn.dribbble.com/users/88000/screenshots/2487367/shot.png")', backgroundSize:'cover', backgroundRepeat:'no-repeat', width: '50vw', height: '100vh'}}></div>
+    <div className='flexXY' style={{width:'100vw', height:'100vh', overflowY:'hidden'}}>
+    {/* <div className='maintenance' style={{backgroundImage: 'url("https://cdn.dribbble.com/users/5481358/screenshots/17990906/media/fd92a679d6a824962260807fe72ff86f.gif")', backgroundSize:'cover', backgroundRepeat:'no-repeat', width: '100vw', height: '100vh'}}></div> */}
+    <img src='https://cdn.dribbble.com/users/125312/screenshots/1419342/media/4fa8e224237f5f04902dfeaf6e4f61ec.gif'/>
     </div>
   )
 }
