d6d95257d27c0160526118c608870b9703a85c00
ayazq18@gmail.com <131868270+ayazq18@users.noreply.github.com>
2024-02-28 16:49:22 +0530

    updated the login comp appearance

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,50 +3,50 @@ import './Login.css'
 import { expandedObj } from "../Constants.js";
 import { useNavigate } from "react-router-dom";
 
-export default function Login({token,setToken, showLogin, setShowLogin, setShowSignUp, showSignUp}) {
-    const MyRef = useRef(null);
-    const navigate = useNavigate()
-      const handleSignOut = ()=>{
-        if(token){
-          setToken('')
-          localStorage.removeItem('token')
-          localStorage.removeItem('name')
-        }
-      }
+export default function Login({ token, setToken, showLogin, setShowLogin, setShowSignUp, showSignUp }) {
+  const MyRef = useRef(null);
+  const navigate = useNavigate()
+  const handleSignOut = () => {
+    if (token) {
+      setToken('')
+      localStorage.removeItem('token')
+      localStorage.removeItem('name')
+    }
+  }
 
-      const handletopage = ()=>{
-        navigate('/ComingSoon')
-      }
+  const handletopage = () => {
+    navigate('/ComingSoon')
+  }
 
   return (
     <div>
-        <div className="navheader-one">
-            <div onClick={()=>{setShowLogin(false)}} className="Expand-to-logout-one flexXY">
-            <div className="logout-profile-one"><h2>My Account</h2></div>
-            <div className="logout-account-one flex">
-                <div className="logout-account-sec">
-                <h6>Account</h6>
-                {expandedObj.map((Obj, index)=>(
-                    <div key={index} onClick={()=>handletopage()} className="account-items-one flexY">
-                    {Obj.icons}
-                    <h4 key={index}>{Obj.item}</h4>
-                    </div>
-                ))}
+      <div className="navheader-one">
+        <div onClick={() => { setShowLogin(false) }} className="Expand-to-logout-one flexXY">
+          <div className="logout-profile-one"><h2>My Account</h2></div>
+          <div className="logout-account-one flex">
+            <div className="logout-account-sec">
+              <h6>Account</h6>
+              {expandedObj.map((Obj, index) => (
+                <div key={index} onClick={() => handletopage()} className="account-items-one flexY">
+                  {Obj.icons}
+                  <h4 key={index}>{Obj.item}</h4>
                 </div>
-                <div className="logout-tools-sec-one">
-                <h6>Quick tools</h6>
-                {expandedObj.map((Obj, index)=>(
-                    <div onClick={()=>handletopage()} className="tools-items-one flexY">
-                    {Obj.toolsIcon}
-                    <h4 key={index}>{Obj.tools}</h4>
-                    </div>
-                ))}
+              ))}
+            </div>
+            <div className="logout-tools-sec-one">
+              <h6>Quick tools</h6>
+              {expandedObj.map((Obj, index) => (
+                <div onClick={() => handletopage()} className="tools-items-one flexY">
+                  {Obj.toolsIcon}
+                  <h4 key={index}>{Obj.tools}</h4>
                 </div>
+              ))}
             </div>
-            <div className="expande-signout-one"><h4 ref={MyRef} onClick={()=>{handleSignOut()}}>Logout</h4></div>
-            </div>
+          </div>
+          <div className="expande-signout-one"><h4 ref={MyRef} onClick={() => { handleSignOut() }}>Logout</h4></div>
         </div>
-        <div className="signUpPage flexXY">
+      </div>
+      <div className="signUpPage flexXY">
       </div>
     </div>
   )
