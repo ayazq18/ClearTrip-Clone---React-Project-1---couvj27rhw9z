0577286620453f1596fd31a1da2e8c3d50a209ed
Ayaz Shahnawaz Qureshi <131868270+ayazq18@users.noreply.github.com>
2024-10-25 01:42:58 +0530

    banner about the intent of the site

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import "./NavBar.css";
 import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import Signup from "../Signup/Signup.js";
-import {logo, flight, flightsidelogo, hotelsidelogo, mytrips, bus, support } from "../../Resources/Icons.js";
+import { logo, flight, flightsidelogo, hotelsidelogo, mytrips, bus, support } from "../../Resources/Icons.js";
 import Login from "../Login/Login.js";
 
 export default function NavBar() {
@@ -12,9 +12,9 @@ export default function NavBar() {
   const [token, setToken] = useState(localStorage.getItem('token'));
 
   const handleSignin = () => {
-    if(token){
+    if (token) {
       setShowLogin(!showLogin)
-    }else{
+    } else {
       setShowSignUp(!showSignup)
     }
   };
@@ -22,64 +22,65 @@ export default function NavBar() {
   const navigate = useNavigate()
   return (
     <div id="navBar-home">
-       <div className="signup">{!showSignup && (<Signup token={token} setToken={setToken} showSignup = {showSignup} setShowSignUp = {setShowSignUp}/>)}</div>
+      <div className="signup">{!showSignup && (<Signup token={token} setToken={setToken} showSignup={showSignup} setShowSignUp={setShowSignUp} />)}</div>
       <div className="navheader-container">
         <div id="navheader" className="c">
-          <div onClick={()=>navigate('/')}>
+          <div onClick={() => navigate('/')}>
             {logo}
           </div>
-            <button className={` flexXY ${token ? 'logout-button' : 'login-Btn'}`} onClick={()=>handleSignin()}>{token ? <><svg viewBox="0 0 14 14" height="16px" width="16px" className="c-inherit"><g fill="none" fillRule="evenodd"><rect width="14" height="14" fill="#FFF" opacity="0"></rect><circle cx="7" cy="7" r="6.25" stroke="currentColor" strokeWidth="1.5"></circle><path fill="currentColor" d="M3,5 C4.38071187,5 5.5,3.88071187 5.5,2.5 C5.5,1.11928813 4.38071187,0 3,0 C1.61928813,0 0.5,1.11928813 0.5,2.5 C0.5,3.88071187 1.61928813,5 3,5 Z" transform="matrix(-1 0 0 1 10 3)"></path><path fill="currentColor" d="M7,9 C9.14219539,9 10.8910789,10.6839685 10.9951047,12.8003597 L11,13 L3,13 C3,10.790861 4.790861,9 7,9 Z"></path><circle cx="7" cy="7" r="7.75" stroke="#FFF" strokeWidth="1.5"></circle></g></svg> {localStorage.getItem('name')}</> : 'Login / Signup'}</button>
-          <div className='login-component'>{showLogin && <Login token={token} setToken={setToken} showLogin={showLogin} setShowLogin={setShowLogin}  showSignup = {showSignup} setShowSignUp = {setShowSignUp}/>}</div>
+          <p style={{color:'green', backgroundColor:'yellow', padding:'5px', fontSize:'12px', width:'630px', textAlign:'center'}}>This site is a clone website. It is not the real, official site. Its purpose is to look like the official site for portfolio purposes. This site is not for active use. Do NOT enter your credentials or share any personal information.</p>
+          <button className={` flexXY ${token ? 'logout-button' : 'login-Btn'}`} onClick={() => handleSignin()}>{token ? <><svg viewBox="0 0 14 14" height="16px" width="16px" className="c-inherit"><g fill="none" fillRule="evenodd"><rect width="14" height="14" fill="#FFF" opacity="0"></rect><circle cx="7" cy="7" r="6.25" stroke="currentColor" strokeWidth="1.5"></circle><path fill="currentColor" d="M3,5 C4.38071187,5 5.5,3.88071187 5.5,2.5 C5.5,1.11928813 4.38071187,0 3,0 C1.61928813,0 0.5,1.11928813 0.5,2.5 C0.5,3.88071187 1.61928813,5 3,5 Z" transform="matrix(-1 0 0 1 10 3)"></path><path fill="currentColor" d="M7,9 C9.14219539,9 10.8910789,10.6839685 10.9951047,12.8003597 L11,13 L3,13 C3,10.790861 4.790861,9 7,9 Z"></path><circle cx="7" cy="7" r="7.75" stroke="#FFF" strokeWidth="1.5"></circle></g></svg> {localStorage.getItem('name')}</> : 'Login / Signup'}</button>
+          <div className='login-component'>{showLogin && <Login token={token} setToken={setToken} showLogin={showLogin} setShowLogin={setShowLogin} showSignup={showSignup} setShowSignUp={setShowSignUp} />}</div>
         </div>
       </div>
       <div className="sideNavBar-container flexXY">
-      <div id="sideNavBar">
-        <div id="sideNavBar-section">
-          <ul id="sideNavBar-links">
-            <li>
+        <div id="sideNavBar">
+          <div id="sideNavBar-section">
+            <ul id="sideNavBar-links">
+              <li>
                 <NavLink to="/flights">
                   <div className="flexY g10">
-                  <div>{flightsidelogo}</div>
-                  <h2 className="aside-links">Flights</h2>
+                    <div>{flightsidelogo}</div>
+                    <h2 className="aside-links">Flights</h2>
                   </div>
                 </NavLink>
-            </li>
-            <li>
-              <NavLink to="/hotel">
-              <div className="flexY g10">
-                  <div>{hotelsidelogo}</div>
-                  <h2 className="aside-links">Hotels</h2>
+              </li>
+              <li>
+                <NavLink to="/hotel">
+                  <div className="flexY g10">
+                    <div>{hotelsidelogo}</div>
+                    <h2 className="aside-links">Hotels</h2>
                   </div>
-              </NavLink>
-            </li>
-            {token && <li>
-              <NavLink to="/myTrips">
-              <div className="flexY g10">
-                  <div>{mytrips}</div>
-                  <h2 className="aside-links">My Trips</h2>
+                </NavLink>
+              </li>
+              {token && <li>
+                <NavLink to="/myTrips">
+                  <div className="flexY g10">
+                    <div>{mytrips}</div>
+                    <h2 className="aside-links">My Trips</h2>
                   </div>
-              </NavLink>
-            </li>}
-            <li>
-              <NavLink to="/Bus">
-              <div className="flexY g10">
-                  <div>{bus}</div>
-                  <h2 className="aside-links">Bus</h2>
+                </NavLink>
+              </li>}
+              <li>
+                <NavLink to="/Bus">
+                  <div className="flexY g10">
+                    <div>{bus}</div>
+                    <h2 className="aside-links">Bus</h2>
                   </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/Support">
-              <div className="flexY g10">
-                  <div>{support}</div>
-                  <h2 className="aside-links">Support</h2>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/Support">
+                  <div className="flexY g10">
+                    <div>{support}</div>
+                    <h2 className="aside-links">Support</h2>
                   </div>
-              </NavLink>
-            </li>
-          </ul>
-          <Outlet />
+                </NavLink>
+              </li>
+            </ul>
+            <Outlet />
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
